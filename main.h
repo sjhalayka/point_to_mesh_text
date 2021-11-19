b1ab40fcb43e9ff8ab4ba5ef2a6501442697866c
Shawn Halayka <sjhalayka@gmail.com>
2021-11-19 14:49:02 -0600

    Add files via upload

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef main_H
 #define main_H
 
-#include "uv_camera.h"
+
 
 #include "marching_cubes.h"
 
@@ -9,7 +9,6 @@
 
 
 #include <cstdlib>
-#include <GL/glut.h>       //GLUT Library
 
 #include <iostream>
 using std::cout;
@@ -45,48 +44,8 @@ using std::pair;
 using std::ios_base;
 
 
-void idle_func(void);
-void init_opengl(const int& width, const int& height);
-void reshape_func(int width, int height);
-void display_func(void);
-void keyboard_func(unsigned char key, int x, int y);
-void mouse_func(int button, int state, int x, int y);
-void motion_func(int x, int y);
-void passive_motion_func(int x, int y);
-
-void render_string(int x, const int y, void* font, const string& text);
-void draw_objects(void);
 
 
-vector<vertex_3> all_points;
-
-
-vertex_3 background_colour(1.0f, 1.0f, 1.0f);
-vertex_3 control_list_colour(0.0f, 0.0f, 0.0f);
-
-bool draw_axis = true;
-bool draw_control_list = true;
-
-uv_camera main_camera;
-
-GLint win_id = 0;
-GLint win_x = 800, win_y = 600;
-float camera_w = 20;
-
-float camera_fov = 45;
-float camera_x_transform = 0;
-float camera_y_transform = 0;
-float u_spacer = 0.01;
-float v_spacer = 0.5 * u_spacer;
-float w_spacer = 0.1;
-float camera_near = 0.1f;
-float camera_far = 10000.0f;
-
-bool lmb_down = false;
-bool mmb_down = false;
-bool rmb_down = false;
-int mouse_x = 0;
-int mouse_y = 0;
 
 
 void add_field_border(vector<float>& values, const float border_value, float& grid_min, float& grid_max, size_t& res)
